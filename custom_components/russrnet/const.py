1530062f9bc955025850daf9b74861300a963dd7
sharespamnow <104461662+sharespamnow@users.noreply.github.com>
2026-07-03 15:23:42 -0400

    Refactor constants in const.py for Russound integration
    Updated default name and constants for zones in Russound RNET integration.

--- custom_components/russrnet/const.py
+++ custom_components/russrnet/const.py
@@ -2,20 +2,15 @@
 
 DOMAIN = "russrnet"
 
-DEFAULT_NAME = "Russound CAM6.6"
+DEFAULT_NAME = "Russound"
 
 CONF_ZONES = "zones"
 CONF_SOURCES = "sources"
 
-# RNET has no fixed default port - depends entirely on your
-# serial-to-TCP bridge configuration (ser2net, GC-100, etc.)
-# Common bridge defaults are 9999 or 8899, but there is no
-# universal standard, so no default is assumed here.
-
 # CAM6.6 supports up to 6 zones and 6 sources per controller.
-MAX_ZONES = 6
+ZONES_PER_CONTROLLER = 6
 MAX_SOURCES = 6
 
-# Russound RNET volume range
+# RNET volume range used by aiorussound's RussoundRNETClient
 MIN_VOLUME = 0
 MAX_VOLUME = 50
